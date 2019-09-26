e475d59d4c50bb1a89e141b6328991edae4e658c
Cedric Roux <cedricroux@outlook.fr>
2019-09-26 23:53:34 +0200

    amelioration du array

--- concerts.php
+++ concerts.php
@@ -1,40 +1,43 @@
 <h1 class="titlebig">Concerts à venir</h1>
 <?php
     $groups = [
-        'Taxi '  =>
-            '<h2> Taxi </h2>',
-                '<p> du 1/11/2019 au 06/11/2019</p>',
-                '<img src="pictures/taxi.jpg" class="artistpictures" alt="notes de musiques">',
-                '<p>Taxi est un groupe de musique australien, composé de 4 jeunes adolescents. Il a été formé en 2015 dans le quartiers de springfield 
-                    pour reprendre « les plus belles chansons célébrant la joie de vivre et l\'insouciance». En mai 2018, le groupe a commencé une 
-                    tournée internationale. Ils seront sur la scene du cailloux durant la premiere semaine du mois de novembre.</p>',
+        'Taxi '  => [
+                'du 1/11/2019 au 06/11/2019',
+                'pictures/taxi.jpg',
+                'Taxi le groupe phenome australien a commencé une 
+                    tournée internationale. Ils seront sur la scene du cailloux durant la premiere semaine du mois de novembre.',
+                ],
+        'Crazy goats ' => [
+                '12/11/2019 ',
+                'pictures/crazygoats.jpg',
+                'Les Crazy goats sont de passage dans la region pour une representation unique reservez
+                 vite vos places il n\'y en aura pas pour tout le monde',
+        ],
+        ' Amstrong\'s sisters ' => [
+                '25/12/2019',
+                'pictures/amstrongssisters2.jpg',
+                'Les jazzy nonnes nous font l\'honneur de leur presence au caillou pensez a prendre vos chaussures
+                               les plus confortables elles vont comme a leur habitude vous faire danser
+                               toute la nuit.',
+        ],
+         'Echo ' => [
+             'du 12/03/2020 14/03/2020',
+             'pictures/echo.jpg',
+             'Pas besoin de presenter Echo. Le trio viendra au caillou pour 3 representations exeptionnelles les 12 ,13 et 14 mars prochain',
+        ],
+    ];
 
-        'Crazy goats ' =>
-            '<h2> Crazy goats </h2>',
-            '<p>12/11/2019</p>',
-            '<img src="pictures/crazygoats.jpg" class="artistpictures" alt="notes de musiques">',
-            '<p>Les Crazy goats sont de passage dans la region pour une representation unique reservez
-                    vite vos places il n\'y en aura pas pour tout le monde</p>',
 
-        ' Amstrong\'s sisters ' =>
-            '<h2> Amstrong\'s sisters </h2>',
-            '<p>25/12/2019</p>',
-            '<img src="pictures/amstrongssisters2.jpg" class="artistpictures" alt="notes de musiques">',
-            '<p>Les jazzy nonnes nous font l\'honneur de leur presence au caillou pensez a prendre vos chaussures
-                       les plus confortables elles vont comme a leur habitude vous faire danser
-                       toute la nuit.</p>',
-
-         'Echo ' =>
-             '<h2> Echo </h2>',
-             '<p>du 12/03/2020 14/03/2020</p>',
-             '<img src="pictures/echo.jpg" class="artistpictures" alt="notes de musiques">',
-             '<p>Pas besoin de presenter Echo. Le trio viendra au caillou pour 3 representations exeptionnelles les 12 ,13 et 14 mars prochain</p>',
-
-        ];
-
-foreach ($groups as $group => $description  ) {
+foreach ($groups as $group => $data  ) {
+    echo '<h2>' . $group . '</h2>';
     echo '<br>';
-    echo $description;
+    echo '<p>' . $data[0] . '</p>';
+    echo '<br>';
+    echo '<img src="' . $data[1] . '" class="artistpictures" alt="photo groupe de musique">';
+    echo '<br>';
+    echo '<p>' . $data[2] . '</p>';
+    echo '<br>';
+
 }
 ?>
 </div>
